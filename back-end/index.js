cdbb62de4f23ab86cfaed86dc627309972eac602
Ibrahem Elsaid <158269891+Ibra7emels3id@users.noreply.github.com>
2025-02-18 14:02:20 +0200

    first commit

--- back-end/index.js
+++ back-end/index.js
@@ -12,7 +12,7 @@ const AllOrder = require('./routes/OrderRouter');
 const AllReview = require('./routes/ReviewRouter');
 
 // cors
-const whitelist = ['http://localhost:3000', 'http://localhost:5000'];
+const whitelist = ['http://localhost:3000', 'https://cental-front-end.vercel.app'];
 app.use(cors({
     origin: whitelist,
     optionsSuccessStatus: 200,
